e4a98f4f2c81d95c9d444f8ec21d5c24bb15bac8
Saeed Physics <sdmi13790@gmail.com>
2022-08-05 13:08:50 +0430

    add pagination

--- books/views.py
+++ books/views.py
@@ -6,6 +6,7 @@ from .models import Book
 
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 6
     template_name = 'books/books_list.html'
     context_object_name = 'books'
 
