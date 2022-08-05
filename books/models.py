91599ce5702ce0ce0698fa8731d1e5b121761e97
Saeed Physics <sdmi13790@gmail.com>
2022-08-05 14:40:48 +0430

    add related filed to book fk in comment

--- books/models.py
+++ books/models.py
@@ -19,9 +19,9 @@ class Book(models.Model):
 
 class Comment(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='comments')
     text = models.TextField()
     datetime_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.user}: {self.text}'
+        return f'{self.text}'
