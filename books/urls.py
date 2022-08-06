3f359837df48c8285951ca1b94cda56a563d3ebe
Saeed Physics <sdmi13790@gmail.com>
2022-08-06 11:46:17 +0430

    render book comments in book detail

--- books/urls.py
+++ books/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
+from .views import BookListView, BookCreateView, BookUpdateView, BookDeleteView, book_detail_view  # BookDetailView,
 
 urlpatterns = [
     path('', BookListView.as_view(), name='book_list_view'),
-    path('<int:pk>/', BookDetailView.as_view(), name='book_detail_view'),
+    # path('<int:pk>/', BookDetailView.as_view(), name='book_detail_view'),
+    path('<int:pk>/', book_detail_view, name='book_detail_view'),
     path('create/', BookCreateView.as_view(), name='book_create_view'),
     path('<int:pk>/update/', BookUpdateView.as_view(), name='book_update_view'),
     path('<int:pk>/delete/', BookDeleteView.as_view(), name='book_delete_view'),
